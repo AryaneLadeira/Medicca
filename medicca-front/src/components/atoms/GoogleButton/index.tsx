eebcb8e48baa75df2a4eb6c1354b4ab98d4e79e7
Aryane Ladeira <aryanemottals@gmail.com>
2025-01-16 02:03:13 -0300

    Fixing button style

--- medicca-front/src/components/atoms/GoogleButton/index.tsx
+++ medicca-front/src/components/atoms/GoogleButton/index.tsx
@@ -2,13 +2,13 @@ import GoogleIcon from '@mui/icons-material/Google';
 import { Button, Typography } from '@mui/material';
 import './style.scss';
 
-export default function GoogleButton() {
+function GoogleButton() {
   return (
-    <Button className="google-login-btn" fullWidth>
+    <Button className="google-login-btn large-btn" fullWidth>
       <GoogleIcon className="google-icon" />
-      <Typography variant="body2">
-        Entrar com o Google
-      </Typography>
+      <Typography variant="body2">Entrar com o Google</Typography>
     </Button>
   );
 }
+
+export default GoogleButton;
